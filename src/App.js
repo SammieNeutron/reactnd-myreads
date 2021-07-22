e4b78fee1eb243ae09c5a2d29defc11883ffce7c
sammieneutron <samsonadegboyega@gmail.com>
2021-07-22 13:32:13 +0100

    Added toast to alert sucessful moving of books

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css'
 import Shelf from './components/Shelf'
 import Search from './components/Search'
 
+import { ToastContainer, toast } from 'react-toastify';
+
 class BooksApp extends React.Component {
     state = {
       books: [],
@@ -13,18 +15,28 @@ class BooksApp extends React.Component {
 
     componentDidMount() {
       BooksAPI.getAll()
-      .then((res) => {
-        console.log(res)
-        this.setState({books: res})
+      .then((data) => {
+        console.log(data)
+        this.setState({books: data})
       })
     }
     
 
     changeBookShelf(book, shelf) {
       BooksAPI.update(book, shelf)
-      .then(() =>  {
-        // console.log(res)
-        this.setState({books: books})
+      .then((data) =>  {
+        console.log(data)
+        this.setState((currState) => ({
+          books: currState.data
+        }))
+      }).catch(() => {
+        toast.success(`${book.title} moved to ${shelf} successfully!`, {
+          position: toast.POSITION.TOP_RIGHT
+      });
+        // alert()
+        setTimeout(() => {
+          window.location.reload();
+      }, 3000);
       })
     }
 
@@ -33,6 +45,7 @@ class BooksApp extends React.Component {
     const { books } = this.state
     return (
       <div className="app">
+        <ToastContainer />
         <Route exact path="/" render={() => 
             <Shelf books={books} onMoveBookToShelf={this.changeBookShelf} />
         } />
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+import 'react-toastify/dist/ReactToastify.css';
+
 ReactDOM.render(
 <BrowserRouter>
 <App /></BrowserRouter>, document.getElementById('root'))
